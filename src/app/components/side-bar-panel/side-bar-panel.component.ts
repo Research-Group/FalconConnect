919b6162adf0e8ce687016a009a715c47c45f5d8
Dylan Hensley <dylanhensley172495@gmail.com>
2018-11-01 00:06:59 -0500

    Side bar toggle

--- src/app/components/side-bar-panel/side-bar-panel.component.ts
+++ src/app/components/side-bar-panel/side-bar-panel.component.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { Component, OnInit } from '@angular/core'
 
 @Component({
@@ -7,8 +8,8 @@ import { Component, OnInit } from '@angular/core'
 })
 export class SideBarPanelComponent implements OnInit {
 
-  profilePanel: boolean = false;
-  classes: boolean = false;
+
+
 
   constructor() {
    }
@@ -17,10 +18,35 @@ export class SideBarPanelComponent implements OnInit {
   }
 
   toggle(panel: string) {
-    if (panel == 'profilePanel')
-      this.profilePanel = !this.profilePanel;
+  // toggles everything off then opens by ID, so far you cannot click to close and leave pane empty.
+  var profile = document.getElementById('profile');
+  var classes = document.getElementById('classes');
+  if(profile.classList.toggle("show")==true && classes.classList.toggle("show")==true){
+    profile.classList.toggle("show");
+    classes.classList.toggle("show");
+  }
+  else if (profile.classList.toggle("show")==true){
+  profile.classList.toggle("show");
+  }
+  else if (classes.classList.toggle("show")==true){
+    classes.classList.toggle("show");
+  }
 
-    var element = document.getElementById(panel);
-    element.classList.toggle("show");
+  if (panel == 'profile'){
+  var element = document.getElementById(panel);
+  element.classList.toggle("show");}
+
+  else if (panel == 'classes') {
+   // element = document.getElementById('profile');
+   // if(element.classList.toggle("show")==true){
+   //  element.classList.toggle("show");
+   // }
+  element = document.getElementById(panel);
+  element.classList.toggle("show");
+
+
+
+          }
+
   }
 }
